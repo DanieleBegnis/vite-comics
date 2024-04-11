3a0a18f171ff5eeea7a00c61dc1f55fb348a6153
DanieleBegnis <daniele.begnis@gmail.com>
2024-04-11 15:55:33 +0200

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script>
+  import AppHeader from './components/AppHeader.vue';
 
+  export default {
+    components: {
+      AppHeader,
+    }
+  }
 </script>
 
 <template>
-  ciao
+  <AppHeader></AppHeader>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+    <div>hello sono header</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
