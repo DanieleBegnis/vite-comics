b961c7bd5a123e672926615a3669d63a8a602eed
DanieleBegnis <daniele.begnis@gmail.com>
2024-04-11 21:56:49 +0200

    feat: add footer links list

--- src/components/AppFooterLinks.vue
+++ src/components/AppFooterLinks.vue
@@ -1,12 +1,137 @@
 <script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+            comics: [
+                {
+                    text: 'Characters',
+                },
+                {
+                    text: 'Comics',
+                },
+                {
+                    text: 'Movies',
+                },
+                {
+                    text: 'TV',
+                },
+                {
+                    text: 'Games',
+                },
+                {
+                    text: 'Videos',
+                },
+                {
+                    text: 'News',
+                }
+            ],
+            shop: [
+                {
+                    text: 'Shop DC'
+                },
+                {
+                    text: 'Shop DC Collectibles'
+                }
+            ],
+            dc: [
+                {
+                    text: 'Terms of use',
+                },
+                {
+                    text: 'Privacy policy',
+                },
+                {
+                    text: 'Ad choices',
+                },
+                {
+                    text: 'Advertising',
+                },
+                {
+                    text: 'Jobs',
+                },
+                {
+                    text: 'Subscription',
+                },
+                {
+                    text: 'Talent workshop',
+                },
+                {
+                    text: 'CPSC certificate',
+                },
+                {
+                    text: 'Ratings',
+                },
+                {
+                    text: 'Shop help',
+                },
+                {
+                    text: 'Contact us',
+                }
+            ],
+            sites: [
+                {
+                    text: 'DC',
+                },
+                {
+                    text: 'MAD magazine',
+                },
+                {
+                    text: 'DC kids',
+                },
+                {
+                    text: 'DC universe',
+                },
+                {
+                    text: 'DC POWER VISA',
+                }
+            ],
+        }
+    }
+}
 </script>
 
 <template>
     <footer>
         <div class="container">
             <div class="row">
-                <div class="col-4 "></div>
-                <div class="col-8 "></div>
+                <div class="col-4 ">
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="subtitle">DC COMICS</div>
+                            <ul>
+                                <li class="px-3 d-flex align-items-center" v-for="comic in comics">
+                                    <a class="px-6" href="#">{{ comic.text }}</a>
+                                </li>
+                            </ul>
+                            <div class="subtitle">SHOP</div>
+                            <ul>
+                                <li class="px-3 d-flex align-items-center" v-for="store in shop">
+                                    <a class="px-6" href="#">{{ store.text }}</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-4">
+                            <div class="subtitle">DC</div>
+                            <ul>
+                                <li class="px-3 d-flex align-items-center" v-for="magazine in dc">
+                                    <a class="px-6" href="#">{{ magazine.text }}</a>
+                                </li>
+                            </ul>                          
+                        </div>
+                        <div class="col-4">
+                            <div class="subtitle">SITES</div>
+                            <ul>
+                                <li class="px-3 d-flex align-items-center" v-for="site in sites">
+                                    <a class="px-6" href="#">{{ site.text }}</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-8 ">
+                    <img src="../assets/img/dc-logo-bg.png" alt="">
+                </div>
             </div>
         </div>
     </footer>
@@ -17,4 +142,14 @@
     footer {
         background-image: url('src/assets/img/footer-bg.jpg');
     }
+    .subtitle {
+        color: #fff;
+    }
+    li {
+        font-size: 10px;
+        a {
+            color: #959595;
+        }
+    }
+    
 </style>
