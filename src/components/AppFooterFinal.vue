e02d58a9c5349b10875659966ed75b78ad70ba45
DanieleBegnis <daniele.begnis@gmail.com>
2024-04-11 22:34:52 +0200

    feat: add array footer

--- src/components/AppFooterFinal.vue
+++ src/components/AppFooterFinal.vue
@@ -1,4 +1,28 @@
 <script>
+export default {
+    name: 'AppFooterFinal',
+    data() {
+        return {
+            links: [
+                {
+                    photo: 'buy-comics-digital-comics.png'
+                },
+                {
+                    photo: 'buy-comics-merchandise.png'
+                },
+                {
+                    photo: 'buy-comics-subscriptions.png'
+                },
+                {
+                    photo: 'buy-comics-shop-locator.png'
+                },
+                {
+                    photo: 'buy-dc-power-visa.svg'
+                },
+            ]
+        }
+    }
+}
 </script>
 
 <template>
@@ -8,7 +32,10 @@
                 <div class="col-6">
                     <button type="button" class="btn my-3">SIGN-UP! NOW</button>
                 </div>
-                <div class="col-6"></div>
+                <div class="col-6 d-flex align-items-center">
+                    <div class="subtitle font-weight-bold fs-5"> FOLLOW US</div>
+                    <ul></ul>
+                </div>
             </div>
         </div>
     </footer>
@@ -22,6 +49,9 @@
             color: #fff;
             border: 3px solid $brand-primary;
             border-radius: 0
+        }
+        .subtitle {
+            color: $brand-primary;
         }
     }
 </style>
