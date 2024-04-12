d0a71dfb2160945522bbe08dd698959211c42f1d
DanieleBegnis <daniele.begnis@gmail.com>
2024-04-12 17:11:33 +0200

    feat: add jumbotron

--- src/components/AppMainContent.vue
+++ src/components/AppMainContent.vue
@@ -84,6 +84,9 @@ export default {
 
 <template>
     <main>
+        <div class="jumbotron">
+
+        </div>
        <div class="my-3 container">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit voluptates nam nulla eius maiores cum saepe, aliquam aliquid voluptatem inventore rerum commodi vel perspiciatis ratione distinctio, quas esse possimus corrupti.
        </div>
@@ -93,8 +96,12 @@ export default {
 <style scoped lang="scss">
  @use '../style/partials/variables' as *;
     main {
-        height: 200px;
         background-color: $brand-secondary;
         color: #fff;
+        .jumbotron {
+            background-image: url('src/assets/img/jumbotron.jpg');
+            background-size: cover;
+            height: 400px;
+        }
     }
 </style>
